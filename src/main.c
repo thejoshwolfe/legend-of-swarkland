9302b62bbde84700135f72b05c3bfe9a7cc9f24d
Josh Wolfe <thejoshwolfe@gmail.com>
2015-01-31 19:57:20 -0700

    isolate freeimage usage to a function

--- src/main.c
+++ src/main.c
@@ -6,60 +6,34 @@
 #include <SDL2/SDL.h>
 #include <FreeImage.h>
 
-static void panic(const char *str) __attribute__ ((noreturn));
-
-static void panic(const char *str)  {
+static void panic(const char * str) __attribute__ ((noreturn));
+static void panic(const char * str) {
     fprintf(stderr, "%s\n", str);
     abort();
 }
 
-static void *panic_malloc(size_t count, size_t size) {
-    void *mem = malloc(size * count);
+static void * panic_malloc(size_t count, size_t size) {
+    void * mem = malloc(size * count);
     if (!mem)
         panic("out of memory");
     return mem;
 }
 
-int main(int argc, char *argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        panic("unable to init SDL");
-    }
-    rucksack_init();
-
-    struct RuckSackBundle *bundle;
-    if (rucksack_bundle_open("build/resources.bundle", &bundle) != RuckSackErrorNone) {
-        panic("error opening resource bundle");
-    }
-    SDL_Window *window = SDL_CreateWindow("Legend of Swarkland",
-            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
-    if (!window) {
-        panic("window create failed");
-    }
-
-    struct RuckSackFileEntry *entry = rucksack_bundle_find_file(bundle, "spritesheet", -1);
-    if (!entry) {
-        panic("spritesheet not found in bundle");
-    }
-
-    struct RuckSackTexture *rs_texture;
-    if (rucksack_file_open_texture(entry, &rs_texture) != RuckSackErrorNone) {
-        panic("open texture failed");
-    }
-
+static SDL_Texture * load_texture(SDL_Renderer * renderer, struct RuckSackTexture * rs_texture) {
     long size = rucksack_texture_size(rs_texture);
-    unsigned char *image_buffer = panic_malloc(size, sizeof(char));
+    unsigned char * image_buffer = panic_malloc(size, sizeof(char));
     if (rucksack_texture_read(rs_texture, image_buffer) != RuckSackErrorNone) {
         panic("read texture failed");
     }
 
-    FIMEMORY *fi_mem = FreeImage_OpenMemory(image_buffer, size);
+    FIMEMORY * fi_mem = FreeImage_OpenMemory(image_buffer, size);
     FREE_IMAGE_FORMAT fif = FreeImage_GetFileTypeFromMemory(fi_mem, 0);
 
     if (fif == FIF_UNKNOWN || !FreeImage_FIFSupportsReading(fif)) {
         panic("unable to decode spritesheet image");
     }
 
-    FIBITMAP *bmp = FreeImage_LoadFromMemory(fif, fi_mem, 0);
+    FIBITMAP * bmp = FreeImage_LoadFromMemory(fif, fi_mem, 0);
     if (!FreeImage_HasPixels(bmp)) {
         panic("spritesheet image has no pixels");
     }
@@ -67,10 +41,7 @@ int main(int argc, char *argv[]) {
     int spritesheet_width = FreeImage_GetWidth(bmp);
     int spritesheet_height = FreeImage_GetHeight(bmp);
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-    SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
-            SDL_TEXTUREACCESS_STATIC, spritesheet_width, spritesheet_height);
+    SDL_Texture * texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, spritesheet_width, spritesheet_height);
     SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_ADD);
 
     SDL_UpdateTexture(texture, NULL, FreeImage_GetBits(bmp), FreeImage_GetPitch(bmp));
@@ -79,9 +50,42 @@ int main(int argc, char *argv[]) {
     FreeImage_CloseMemory(fi_mem);
     free(image_buffer);
 
+    return texture;
+}
+
+int main(int argc, char * argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        panic("unable to init SDL");
+    }
+    rucksack_init();
+
+    struct RuckSackBundle * bundle;
+    if (rucksack_bundle_open("build/resources.bundle", &bundle) != RuckSackErrorNone) {
+        panic("error opening resource bundle");
+    }
+    SDL_Window * window = SDL_CreateWindow("Legend of Swarkland", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+    if (!window) {
+        panic("window create failed");
+    }
+    SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        panic("renderer create failed");
+    }
+
+    struct RuckSackFileEntry * entry = rucksack_bundle_find_file(bundle, "spritesheet", -1);
+    if (!entry) {
+        panic("spritesheet not found in bundle");
+    }
+
+    struct RuckSackTexture * rs_texture;
+    if (rucksack_file_open_texture(entry, &rs_texture) != RuckSackErrorNone) {
+        panic("open texture failed");
+    }
+
+    SDL_Texture * texture = load_texture(renderer, rs_texture);
+
     long image_count = rucksack_texture_image_count(rs_texture);
-    struct RuckSackImage **spritesheet_images = panic_malloc(image_count,
-            sizeof(struct RuckSackImage*));
+    struct RuckSackImage ** spritesheet_images = panic_malloc(image_count, sizeof(struct RuckSackImage*));
     rucksack_texture_get_images(rs_texture, spritesheet_images);
     if (image_count != 1) {
         panic("code only handles 1 image at the moment");
@@ -91,20 +95,20 @@ int main(int argc, char *argv[]) {
     bool running = true;
     while (running) {
         SDL_Event event;
-        while(SDL_PollEvent(&event)) {
+        while (SDL_PollEvent(&event)) {
             switch (event.type) {
-            case SDL_KEYDOWN:
-                switch (event.key.keysym.scancode) {
-                case SDL_SCANCODE_ESCAPE:
+                case SDL_KEYDOWN:
+                    switch (event.key.keysym.scancode) {
+                        case SDL_SCANCODE_ESCAPE:
+                            running = false;
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case SDL_QUIT:
                     running = false;
                     break;
-                default:
-                    break;
-                }
-                break;
-            case SDL_QUIT:
-                running = false;
-                break;
             }
         }
 
@@ -123,7 +127,7 @@ int main(int argc, char *argv[]) {
         SDL_RenderCopyEx(renderer, texture, &source_rect, &dest_rect, 0.0, NULL, SDL_FLIP_VERTICAL);
 
         SDL_RenderPresent(renderer);
-        SDL_Delay(17);
+        SDL_Delay(17); // 60Hz or whatever
     }
 
     free(spritesheet_images);
@@ -140,5 +144,4 @@ int main(int argc, char *argv[]) {
     SDL_Quit();
     return 0;
 }
-
 
