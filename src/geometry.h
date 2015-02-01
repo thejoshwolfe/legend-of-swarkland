f8e665297bbdaa3e4482c3eb97c02a9fe8d776e3
Josh Wolfe <thejoshwolfe@gmail.com>
2015-01-31 22:30:31 -0700

    somebody's goin die

---
+++ src/geometry.h
@@ -0,0 +1,9 @@
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
+
+typedef struct {
+    int x;
+    int y;
+} Coord;
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "load_image.h"
 #include "util.h"
+#include "geometry.h"
 
 #include <stdbool.h>
 
@@ -7,10 +8,9 @@
 #include <SDL2/SDL.h>
 
 static const int tile_size = 32;
-static const int map_size_x = 55;
-static const int map_size_y = 30;
+static const Coord map_size = { 55, 30 };
 
-static void render_tile(SDL_Renderer * renderer, SDL_Texture * texture, struct RuckSackImage * guy_image, int x, int y) {
+static void render_tile(SDL_Renderer * renderer, SDL_Texture * texture, struct RuckSackImage * guy_image, Coord coord) {
     SDL_Rect source_rect;
     source_rect.x = guy_image->x;
     source_rect.y = guy_image->y;
@@ -18,8 +18,8 @@ static void render_tile(SDL_Renderer * renderer, SDL_Texture * texture, struct R
     source_rect.h = guy_image->height;
 
     SDL_Rect dest_rect;
-    dest_rect.x = x * tile_size;
-    dest_rect.y = y * tile_size;
+    dest_rect.x = coord.x * tile_size;
+    dest_rect.y = coord.y * tile_size;
     dest_rect.w = tile_size;
     dest_rect.h = tile_size;
 
@@ -27,25 +27,45 @@ static void render_tile(SDL_Renderer * renderer, SDL_Texture * texture, struct R
 }
 
 static struct RuckSackImage * find_image(struct RuckSackImage ** spritesheet_images, long image_count, const char * name) {
-    for (int i = 0; i< image_count; i++)
+    for (int i = 0; i < image_count; i++)
         if (strcmp(spritesheet_images[i]->key, name) == 0)
             return spritesheet_images[i];
     panic("sprite not found");
 }
 
-static int you_x = 4;
-static int you_y = 4;
-static int bad_x = 10;
-static int bad_y = 10;
-static void move_the_bad(void) {
-    int dx = sign(bad_x - you_x);
-    int dy = sign(bad_y - you_y);
-    bad_x -= dx;
-    bad_y -= dy;
+static void attack(int damage, int * hitpoints, bool * alive) {
+    *hitpoints -= damage;
+    if (*hitpoints <= 0) {
+        *hitpoints = 0;
+        *alive = false;
+    }
 }
-static void move(int dx, int dy){
-    you_x = clamp(you_x + dx, 0, map_size_x - 1);
-    you_y = clamp(you_y + dy, 0, map_size_y - 1);
+
+static Coord you_position = { 4, 4 };
+static Coord badguy_position = { 10, 10 };
+static int you_attack = 3;
+static int you_hitpoints = 10;
+static bool you_alive = true;
+static int badguy_attack = 2;
+static int badguy_hitpoints = 10;
+static bool badguy_alive = true;
+static void move_the_bad(void) {
+    int dx = sign(you_position.x - badguy_position.x);
+    int dy = sign(you_position.y - badguy_position.y);
+    Coord new_position = { clamp(badguy_position.x + dx, 0, map_size.x - 1), clamp(badguy_position.y + dy, 0, map_size.y - 1) };
+    if (new_position.x == you_position.x && new_position.y == you_position.y) {
+        attack(badguy_attack, &you_hitpoints, &you_alive);
+    } else {
+        badguy_position = new_position;
+    }
+}
+static void you_move(int dx, int dy) {
+    Coord new_position = { clamp(you_position.x + dx, 0, map_size.x - 1), clamp(you_position.y + dy, 0, map_size.y - 1) };
+    if (new_position.x == badguy_position.x && new_position.y == badguy_position.y) {
+        attack(you_attack, &badguy_hitpoints, &badguy_alive);
+    } else {
+        you_position = new_position;
+    }
 
     move_the_bad();
 }
@@ -56,7 +76,7 @@ int main(int argc, char * argv[]) {
     }
     rucksack_init();
 
-    SDL_Window * window = SDL_CreateWindow("Legend of Swarkland", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, map_size_x * tile_size, map_size_y * tile_size, 0);
+    SDL_Window * window = SDL_CreateWindow("Legend of Swarkland", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, map_size.x * tile_size, map_size.y * tile_size, 0);
     if (!window) {
         panic("window create failed");
     }
@@ -84,8 +104,8 @@ int main(int argc, char * argv[]) {
     long image_count = rucksack_texture_image_count(rs_texture);
     struct RuckSackImage ** spritesheet_images = panic_malloc(image_count, sizeof(struct RuckSackImage*));
     rucksack_texture_get_images(rs_texture, spritesheet_images);
-    struct RuckSackImage * guy_image =  find_image(spritesheet_images, image_count, "img/guy.png");
-    struct RuckSackImage * bad_image =  find_image(spritesheet_images, image_count, "img/bad.png");
+    struct RuckSackImage * guy_image = find_image(spritesheet_images, image_count, "img/guy.png");
+    struct RuckSackImage * bad_image = find_image(spritesheet_images, image_count, "img/bad.png");
 
     bool running = true;
     while (running) {
@@ -98,16 +118,16 @@ int main(int argc, char * argv[]) {
                             running = false;
                             break;
                         case SDL_SCANCODE_LEFT:
-                            move(-1, 0);
+                            you_move(-1, 0);
                             break;
                         case SDL_SCANCODE_UP:
-                            move(0, -1);
+                            you_move(0, -1);
                             break;
                         case SDL_SCANCODE_RIGHT:
-                            move(1, 0);
+                            you_move(1, 0);
                             break;
                         case SDL_SCANCODE_DOWN:
-                            move(0, 1);
+                            you_move(0, 1);
                             break;
                         default:
                             break;
@@ -121,8 +141,10 @@ int main(int argc, char * argv[]) {
 
         SDL_RenderClear(renderer);
 
-        render_tile(renderer, texture, guy_image, you_x, you_y);
-        render_tile(renderer, texture, bad_image, bad_x, bad_y);
+        if (you_alive)
+            render_tile(renderer, texture, guy_image, you_position);
+        if (badguy_alive)
+            render_tile(renderer, texture, bad_image, badguy_position);
 
         SDL_RenderPresent(renderer);
         SDL_Delay(17); // 60Hz or whatever
