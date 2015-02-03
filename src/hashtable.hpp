24dadd0df5d9040bf9e045838e1c360ed665836e
Josh Wolfe <thejoshwolfe@gmail.com>
2015-02-03 05:53:52 -0700

    upgrading Hashtable to LinkedHashtable.
    This is necessary to have deterministic iteration order over the global list of individuals.

--- src/hashtable.hpp
+++ src/hashtable.hpp
@@ -43,58 +43,101 @@ static inline uint_oversized<Size64> random_oversized() {
     return result;
 }
 
-// uses linear probing
+// sorry. this isn't supposed to be visible.
+template <typename K, typename V>
+struct LinkedHashtableEntry {
+    K key;
+    V value;
+    bool is_valid;
+    LinkedHashtableEntry * previous;
+    LinkedHashtableEntry * next;
+};
+
+// A LinkedList and a Hashtable at the same time.
+// iteration preserves insertion order.
 template<typename K, typename V, int (*HashFunction)(K)>
-class Hashtable {
+class LinkedHashtable {
 public:
-    Hashtable() {
+    LinkedHashtable() {
         init_capacity(16);
     }
-    ~Hashtable() {
+    ~LinkedHashtable() {
         delete[] _table;
     }
     int size() const {
         return _size;
     }
+    // the key must not already be present
     void put(K key, V value) {
         just_put(key, value);
         // if we get too full (75%), double the capacity
         if (_size * 4 >= _capacity * 3) {
-            Entry * old_table = _table;
-            int old_capacity = _capacity;
+            LinkedHashtableEntry<K, V> * old_table = _table;
+            LinkedHashtableEntry<K, V> * old_head = _head;
             init_capacity(_capacity * 2);
-            // dump all the old elements into the new table
-            for (int i = 0; i < old_capacity; i++) {
-                if (old_table[i].is_valid)
-                    just_put(old_table[i].key, old_table[i].value);
-            }
+            // load all the old elements into the new table in the proper order
+            for (LinkedHashtableEntry<K, V> * cursor = old_head; cursor != NULL; cursor = cursor->next)
+                just_put(cursor->key, cursor->value);
             delete[] old_table;
         }
     }
+    // the key must be present
     void remove(K key) {
-        find_entry(key).is_valid = false;
+        LinkedHashtableEntry<K, V> & entry = find_entry(key);
+        entry.is_valid = false;
         _size--;
+        // join the two neighbors
+        if (entry.previous != NULL) {
+            entry.previous->next = entry.next;
+        } else {
+            _head = entry.next;
+        }
+        if (entry.next != NULL) {
+            entry.next->previous = entry.previous;
+        } else {
+            _tail = entry.previous;
+        }
     }
+    // the key must be present
     V get(K key) {
         return find_entry(key).value;
     }
-private:
-    struct Entry {
-        K key;
-        V value;
-        bool is_valid;
+    class Iterator {
+    public:
+        bool has_next() {
+            return cursor != NULL;
+        }
+        V next() {
+            V result = cursor->value;
+            cursor = cursor->next;
+            return result;
+        }
+    private:
+        LinkedHashtableEntry<K, V> * cursor;
+        Iterator(LinkedHashtableEntry<K, V> * cursor) :
+                cursor(cursor) {
+        }
+        friend LinkedHashtable;
     };
+    Iterator value_iterator() {
+        return Iterator(_head);
+    }
+private:
     int _size;
     int _capacity;
-    Entry * _table;
+    LinkedHashtableEntry<K, V> * _table;
+    LinkedHashtableEntry<K, V> * _head;
+    LinkedHashtableEntry<K, V> * _tail;
     void init_capacity(int new_capacity) {
         _size = 0;
         _capacity = new_capacity;
-        _table = new Entry[_capacity];
+        _table = new LinkedHashtableEntry<K, V>[_capacity];
         for (int i = 0; i < _capacity; i++)
             _table[i].is_valid = false;
+        _head = NULL;
+        _tail = NULL;
     }
-    Entry & find_entry(K key) {
+    LinkedHashtableEntry<K, V> & find_entry(K key) {
         int start_index = HashFunction(key) % _capacity;
         for (int roll_over = 0; roll_over < _capacity; roll_over++) {
             int index = (start_index + roll_over) % _capacity;
@@ -105,20 +148,29 @@ private:
     }
     void just_put(K key, V value) {
         int start_index = HashFunction(key) % _capacity;
+        // resolve hash collisions with linear probing.
         for (int roll_over = 0; roll_over < _capacity; roll_over++) {
             int index = (start_index + roll_over) % _capacity;
             if (_table[index].is_valid && _table[index].key != key)
                 continue;
             // found where to put it
             if (_table[index].is_valid) {
-                // we're overwriting an old value with the same key
-            } else {
-                // we're adding an entry
-                _size++;
+                panic("hashtable put overwrites existing value");
             }
             _table[index] = {key, value, true};
+            _size++;
+            if (_tail == NULL) {
+                _head = &_table[index];
+                _table[index].previous = NULL;
+            } else {
+                _tail->next = &_table[index];
+                _table[index].previous = _tail;
+            }
+            _table[index].next = NULL;
+            _tail = &_table[index];
             return;
         }
+        // we're always supposed to maintain some space.
         panic("put into a full hashtable");
     }
 };
