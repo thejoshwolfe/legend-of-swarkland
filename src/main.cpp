8473fe6a5c92a75e6d39067203f364e5e43958c2
Josh Wolfe <thejoshwolfe@gmail.com>
2016-01-11 22:17:28 -0700

    cli help

--- src/main.cpp
+++ src/main.cpp
@@ -12,28 +12,53 @@
 
 #include <SDL.h>
 
+static void help(const char * error_message, const char * argument) {
+    if (error_message != nullptr) {
+        fprintf(stderr, "%s", error_message);
+        if (argument != nullptr) {
+            fprintf(stderr, " %s", argument);
+        }
+        fprintf(stderr, "\n");
+    }
+    fprintf(stderr,
+        "usage: [options...]\n"
+        "\n"
+        "  --dump-script path.swarklandtas\n"
+        "    record game to the specified file.\n"
+        "\n"
+        "  --script path.swarklandtas\n"
+        "    playback game from the specified file.\n"
+        "\n"
+        "  --tas-delay N\n"
+        "    wait N frames (at 60Hz) between each step of the replay.\n"
+        "\n"
+        "");
+    exit(1);
+}
 static void process_argv(int argc, char * argv[]) {
     for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "--dump-script") == 0) {
+        if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0) {
+            help(nullptr, nullptr);
+        } else if (strcmp(argv[i], "--dump-script") == 0) {
             if (i + 1 >= argc)
-                panic("expected argument");
+                help("expected argument after", argv[i]);
             i++;
             char * filename = argv[i];
             tas_set_output_script(filename);
         } else if (strcmp(argv[i], "--script") == 0) {
             if (i + 1 >= argc)
-                panic("expected argument");
+                help("expected argument after", argv[i]);
             i++;
             char * filename = argv[i];
             tas_set_input_script(filename);
         } else if (strcmp(argv[i], "--tas-delay") == 0) {
             if (i + 1 >= argc)
-                panic("expected argument");
+                help("expected argument", argv[i]);
             i++;
             const char * delay_str = argv[i];
             sscanf(delay_str, "%d", &tas_delay);
         } else {
-            panic("unrecognized parameter");
+            help("unrecognized parameter:", argv[i]);
         }
     }
 }
