e0eabc7a0d0b2cbfb63c6d7829d570125526024d
Josh Wolfe <thejoshwolfe@gmail.com>
2018-07-08 15:39:29 -0400

    unused code

--- src/main.zig
+++ src/main.zig
@@ -14,18 +14,6 @@ const SDL_WINDOWPOS_UNDEFINED = @bitCast(c_int, c.SDL_WINDOWPOS_UNDEFINED_MASK);
 
 extern fn SDL_PollEvent(event: *c.SDL_Event) c_int;
 
-// SDL_RWclose is fundamentally unrepresentable in Zig, because `ctx` is
-// evaluated twice. One could make the case that this is a bug in SDL,
-// especially since the docs list a real function prototype that would not
-// have this double-evaluation of the parameter.
-// If SDL would instead of a macro use a static inline function,
-// it would resolve the SDL bug as well as make the function visible to Zig
-// and to debuggers.
-// SDL_rwops.h:#define SDL_RWclose(ctx)        (ctx)->close(ctx)
-inline fn SDL_RWclose(ctx: [*]c.SDL_RWops) c_int {
-    return ctx[0].close.?(ctx);
-}
-
 pub fn main() void {
     if (build_options.headless) return;
     if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
@@ -33,7 +21,14 @@ pub fn main() void {
     }
     defer c.SDL_Quit();
 
-    const screen = c.SDL_CreateWindow(c"My Game Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, spritesheet.width, spritesheet.height, c.SDL_WINDOW_OPENGL) orelse {
+    const screen = c.SDL_CreateWindow(
+        c"Leged of Swarkland",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        spritesheet.width,
+        spritesheet.height,
+        c.SDL_WINDOW_OPENGL,
+    ) orelse {
         std.debug.panic("SDL_CreateWindow failed: {c}\n", c.SDL_GetError());
     };
     defer c.SDL_DestroyWindow(screen);
