6a4b92c434452af6a74dc86545b59939180e598a
Josh Wolfe <thejoshwolfe@gmail.com>
2018-11-07 21:38:57 -0500

    use stdio instead of async sockets

--- src-core/game_engine_client.zig
+++ src-core/game_engine_client.zig
@@ -3,10 +3,9 @@ const std = @import("std");
 pub const GameEngine = struct.{
     child_process: *std.os.ChildProcess,
 
-    socket_file: std.os.File,
-    in_adapter: std.io.FileInStream,
+    in_adapter: std.os.File.InStream,
     in_stream: *std.io.InStream(std.os.File.ReadError),
-    out_adapter: std.io.FileOutStream,
+    out_adapter: std.os.File.OutStream,
     out_stream: *std.io.OutStream(std.os.File.WriteError),
     position: i32,
 
@@ -19,25 +18,13 @@ pub const GameEngine = struct.{
         const args = []const []const u8.{path};
         self.child_process = try std.os.ChildProcess.init(args, std.heap.c_allocator);
         self.child_process.stdout_behavior = std.os.ChildProcess.StdIo.Pipe;
+        self.child_process.stdin_behavior = std.os.ChildProcess.StdIo.Pipe;
         try self.child_process.spawn();
-        var stdout = self.child_process.stdout.?;
-        var port_buffer = []u8.{0} ** 5;
-        const bytes_read = try stdout.read(port_buffer[0..]);
-        std.debug.assert(bytes_read == 5);
-        std.debug.assert(port_buffer[4] == '\n');
-        const port_number = try std.fmt.parseInt(u16, port_buffer[0..4], 16);
-
-        std.debug.warn("connecting to port {}...", port_number);
-        var socket_fd = try std.os.posixSocket(std.os.posix.AF_INET, std.os.posix.SOCK_STREAM | std.os.posix.SOCK_CLOEXEC, std.os.posix.PROTO_tcp);
-        var addr = std.net.Address.initIp4(std.net.parseIp4("127.0.0.1") catch unreachable, port_number);
-        try std.os.posixConnect(socket_fd, addr.os_addr);
-        std.debug.warn("done\n");
-
-        self.socket_file = std.os.File.openHandle(socket_fd);
-        self.in_adapter = std.io.FileInStream.init(self.socket_file);
+        self.in_adapter = self.child_process.stdout.?.inStream();
         self.in_stream = &self.in_adapter.stream;
-        self.out_adapter = std.io.FileOutStream.init(self.socket_file);
+        self.out_adapter = self.child_process.stdin.?.outStream();
         self.out_stream = &self.out_adapter.stream;
+
         self.position = 0;
     }
 
--- src-core/main.zig
+++ src-core/main.zig
@@ -11,83 +11,23 @@ pub fn main() error!void {
 }
 
 fn headlessMain() !void {
-    var stdout_file = try std.io.getStdOut();
-    var stdout_file_out_stream = std.io.FileOutStream.init(stdout_file);
-    var stdout_stream = &stdout_file_out_stream.stream;
-
-    var addr = std.net.Address.initIp4(std.net.parseIp4("127.0.0.1") catch unreachable, 0);
-    var loop: std.event.Loop = undefined;
-    try loop.initSingleThreaded(std.heap.c_allocator);
-
-    var server = @"💩Server".{ .tcp_server = std.event.tcp.Server.init(&loop) };
-    defer server.tcp_server.deinit();
-    try server.tcp_server.listen(addr, @"💩Server".handler);
-
-    try stdout_stream.print("{x4}\n", u16(server.tcp_server.listen_address.port()));
-
-    loop.run();
-}
-
-const @"💩Server" = struct.{
-    tcp_server: std.event.tcp.Server,
-
-    const Self = @This();
-    async<*std.mem.Allocator> fn handler(tcp_server: *std.event.tcp.Server, _addr: *const std.net.Address, _socket: *const std.os.File) void {
-        const self = @fieldParentPtr(Self, "tcp_server", tcp_server);
-        var socket = _socket.*;
-        defer socket.close();
-        // TODO guarantee elision of this allocation
-        const next_handler = async mainLoop(tcp_server.loop, socket) catch unreachable;
-        (await next_handler) catch |err| {
-            std.debug.panic("unable to handle connection: {}\n", err);
-        };
-        suspend {
-            cancel @handle();
-        }
-    }
-};
-async fn mainLoop(loop: *std.event.Loop, socket: std.os.File) !void {
-    var out_adapter = std.io.FileOutStream.init(socket);
-    var out_stream = &out_adapter.stream;
-
-    var in_adapter = std.io.FileInStream.init(socket);
+    var in_adapter = (try std.io.getStdIn()).inStream();
     var in_stream = &in_adapter.stream;
+    var out_adapter = (try std.io.getStdOut()).outStream();
+    var out_stream = &out_adapter.stream;
 
     var position: i32 = 0;
     while (true) {
         {
             var buffer: [1]u8 = undefined;
-            try await try async socketRead(loop, socket.handle, buffer[0..]);
+            try in_stream.readNoEof(buffer[0..]);
             const direction = @bitCast(i8, buffer[0]);
             position += direction;
         }
         {
             var buffer: [4]u8 = undefined;
             std.mem.writeIntBE(i32, &buffer, position);
-            try await try async socketWrite(loop, socket.handle, buffer[0..]);
+            try out_stream.write(buffer[0..]);
         }
-    }
-}
-
-async fn socketRead(loop: *std.event.Loop, fd: std.os.FileHandle, buffer: []u8) !void {
-    while (true) {
-        return std.os.posixRead(fd, buffer) catch |err| switch (err) {
-            error.WouldBlock => {
-                try await try async loop.linuxWaitFd(fd, std.os.posix.EPOLLET | std.os.posix.EPOLLIN);
-                continue;
-            },
-            else => return err,
-        };
-    }
-}
-async fn socketWrite(loop: *std.event.Loop, fd: std.os.FileHandle, buffer: []const u8) !void {
-    while (true) {
-        return std.os.posixWrite(fd, buffer) catch |err| switch (err) {
-            error.WouldBlock => {
-                try await try async loop.linuxWaitFd(fd, std.os.posix.EPOLLET | std.os.posix.EPOLLOUT);
-                continue;
-            },
-            else => return err,
-        };
     }
 }
